b7835ae6d772f38e8c057eca4fdc4a8277aee144
lastninja <lastninja>
2002-05-02 16:03:56 +0000

    added CRC16-Check

--- jHDF/src/tuxonhumax/tools/jHDF/HdfDataBlock.java
+++ jHDF/src/tuxonhumax/tools/jHDF/HdfDataBlock.java
@@ -3,25 +3,28 @@ package tuxonhumax.tools.jHDF;
 import java.io.RandomAccessFile;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import lha.CRC16;
 
 public class HdfDataBlock
 {
 	// Offsets relative to the block's begin
 	private int offNextOffset = 0;
 	private int offBlockChecksum = 2;
-	private int offBlockCompressed = 4;
-	private int offBlockType = 5;
+	private int offBlockType = 4;
 	private int offBlockLength = 6;
-	private int offBlockMemoryAddress = 9;
+	private int offBlockMemoryAddress = 8;
 	private int offData = 12;
 	
 	private byte[] nextOffset = new byte[2];
 	private byte[] blockChecksum = new byte[2];
 	private byte[] blockCompressed = new byte[1];
-	private byte[] blockType = new byte[1];
+	private byte[] blockType = new byte[2];
 	private byte[] blockLength = new byte[2];
-	private byte[] blockMemoryAddress = new byte[3];
+	private byte[] blockMemoryAddress = new byte[4];
 	private byte[] data = null;
+
+    // thats the calculated Checksum: CRC16 of the block
+    private int blockChecksumCalc;
 	
 	private int blockNumber = 0;
 	private int beginOfBlock = 0;
@@ -33,11 +36,12 @@ public class HdfDataBlock
 		nextOffset[0] = 0; nextOffset[1] = 0;
 		blockChecksum[0] = 0; blockChecksum[1] = 0;
 		blockCompressed[0] = 0;
-		blockType[0] = 0;
+		blockType[0] = 0; blockType[1] = 0;
 		blockLength[0] = 0; blockLength[1] = 0;
-		blockMemoryAddress[0] = 0; blockMemoryAddress[1] = 0; blockMemoryAddress[2] = 0;
+		blockMemoryAddress[0] = 0; blockMemoryAddress[1] = 0; blockMemoryAddress[2] = 0; blockMemoryAddress[3] = 0;
 		data = null;
-		
+        
+		blockChecksumCalc = 0;
 		beginOfBlock = 0;
 	}
 
@@ -54,8 +58,10 @@ public class HdfDataBlock
 
 		report.append("Block (" + (blockNumber+1) + ") found at Position 0x" + new Long(0).toHexString(beginOfBlock).toUpperCase() + "\n");
 		report.append("Next Offset      : " + getNextOffset() + "\n");
-		report.append("Block Checksum   : 0x" + Integer.toHexString(getBlockChecksum()).toUpperCase() + "\n");
-		report.append("Block Type       : " + getBlockType() + "\n");
+		report.append("Block Checksum   : 0x" + Integer.toHexString(getBlockChecksum()).toUpperCase());
+        report.append(isBlockChecksumValid() ? " (ok)\n" : "\n");
+		report.append("Block Type       : " + getBlockType());
+        report.append(isBlockCompressed() ? " (compressed)\n" : " (plain)\n");
 		report.append("Block length     : " + getBlockLength() + "\n");
 		report.append("Memory Position  : 0x" + Integer.toHexString(getBlockMemoryAddress()).toUpperCase() + "\n\n");
 		
@@ -83,14 +89,34 @@ public class HdfDataBlock
 		
 			hdfFile.read(nextOffset);
 			hdfFile.read(blockChecksum);
-			blockCompressed[0] = hdfFile.readByte();
-			blockType[0] = hdfFile.readByte();
+			hdfFile.read(blockType);
 			hdfFile.read(blockLength);
-			
-			hdfFile.skipBytes(1);
-			
 			hdfFile.read(blockMemoryAddress);
 			
+            // Calculate the actual Checksum of the block
+            // this value can than be compared with the given checksum 
+            CRC16 actualChecksum = new CRC16();
+            
+            int crcDataStart = beginOfBlock + offBlockType;
+            
+            int crcDataEnd = beginOfBlock + getNextOffset() + 2;
+            
+            byte[] blockData = new byte[crcDataEnd - crcDataStart];
+            hdfFile.seek(crcDataStart);
+            hdfFile.read(blockData);
+            
+            hdfFile.seek(beginOfBlock + 4);
+            hdfFile.read(blockData);
+            
+            actualChecksum.update(blockData,0,crcDataEnd - crcDataStart);
+            
+            blockChecksumCalc = (int)actualChecksum.getValue();
+            
+            /*System.out.println("start: " + Integer.toHexString(crcDataStart));
+            System.out.println("ende: " + Integer.toHexString(crcDataEnd));            
+            System.out.println("länge: " + (crcDataEnd-crcDataStart));
+            System.out.println("actual CRC: " + Integer.toHexString(blockChecksumCalc));*/
+            
  			hdfFile.close();
 		}
 		catch(FileNotFoundException e){}
@@ -119,15 +145,27 @@ public class HdfDataBlock
 
 	public int getBlockMemoryAddress()
 	{
-		return (int) ((blockMemoryAddress[0]<< 16 & 0xFFFFFF)
-					  |(blockMemoryAddress[1]<< 8  & 0xFFFF)
-					  |(blockMemoryAddress[2]      & 0xFF));					  
+		return (int) ((blockMemoryAddress[0]<< 24 & 0xFFFFFFFF)
+                      |(blockMemoryAddress[1]<< 16 & 0xFFFFFF)
+					  |(blockMemoryAddress[2]<< 8  & 0xFFFF)
+					  |(blockMemoryAddress[3]      & 0xFF));					  
 	}
 
 	public int getBlockType()
 	{
-		return (int) (blockType[0] & 0xFF);
+		return (int) (blockType[1] & 0xFF);
 	}
+
+    public int getBlockCompressed()
+    {
+        return (int) (blockType[0] & 0xFF);
+    }
+    
+    public boolean isBlockCompressed()
+    {
+        return (( getBlockCompressed() == 0x80 
+                || getNextOffset() < getBlockLength() ) ? true : false);
+    }
 	
 	public int getBlockLength()
 	{
@@ -145,4 +183,10 @@ public class HdfDataBlock
 		this.blockNumber = blockNumber;
 	}
 
+    public boolean isBlockChecksumValid()
+    {
+        return (getBlockChecksum()==blockChecksumCalc);
+    }
+
 }
+
--- jHDF/src/tuxonhumax/tools/jHDF/HdfHeader.java
+++ jHDF/src/tuxonhumax/tools/jHDF/HdfHeader.java
@@ -3,41 +3,43 @@ package tuxonhumax.tools.jHDF;
 import java.io.RandomAccessFile;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import lha.CRC16;
 
 public class HdfHeader
 {
     private RandomAccessFile hdfFile = null;
 	
 	private String hdfFileName = "";
-	// 00h: maybe filler
-    // Headerlength:    01h
-    private byte[] headerLength = new byte[1];
-    // Headerchecksum:  02h - 03h
+    // Headerlength:    00h - 01h
+    private byte[] headerLength = new byte[2];
+    // Headerchecksum:  02h - 03h --> thats the Checksum to be (as saved in hdf-file)
     private byte[] headerChecksum = new byte[2];
-	// 04h: maybe filler
-    // ModelType:		05h
-    private byte[] modelType = new byte[1];
-	// 06h: maybe filler
-    // Number ob Blocks:07h
-    private byte[] numberBlocks = new byte[1];
+    // ModelType:		04h - 05h
+    private byte[] modelType = new byte[2];
+    // Number ob Blocks:06h - 07h
+    private byte[] numberBlocks = new byte[2];
     // SystemID1:		08h - 0Bh
     private byte[] systemID1 = new byte[4];
     // SystemID2:		0Ch - 0Fh
     private byte[] systemID2 = new byte[4];
-    // 10h: maybe filler
-    // Data Length:		11h - 13h
-    private byte[] dataLength = new byte[3];
+    // Data Length:		10h - 13h
+    private byte[] dataLength = new byte[4];
 
+    // thats the calculated Checksum: CRC16 of Header
+    private int headerChecksumCalc;
+    
 	public HdfHeader()
 	{
 		hdfFileName = "";			
-        headerLength[0] = 0;
+        headerLength[0] = 0; headerLength[1] = 0;
         headerChecksum[0] = 0; headerChecksum[1] = 0;
-	    modelType[0] = 0;
+	    modelType[0] = 0; modelType[1] = 0;
 	    systemID1[0] = 0; systemID1[1] = 0; systemID1[2] = 0; systemID1[3] = 0;	    
 	    systemID2[0] = 0; systemID2[1] = 0; systemID2[2] = 0; systemID2[3] = 0;	    
-	    numberBlocks[0] = 0;
-	    dataLength[0] = 0; dataLength[1] = 0; dataLength[2] = 0;	    
+	    numberBlocks[0] = 0; numberBlocks[1] = 0;
+	    dataLength[0] = 0; dataLength[1] = 0; dataLength[2] = 0; dataLength[3] = 0;
+        
+        headerChecksumCalc = 0;
 	}
     
 	public void setHdfFileName(String fileName)
@@ -53,27 +55,32 @@ public class HdfHeader
 		{
 			hdfFile = new RandomAccessFile(hdfFileName,"r");
 			
-			hdfFile.seek(0x01);
-			headerLength[0] = hdfFile.readByte();
+			hdfFile.read(headerLength);
 			
 			headerChecksum[0] = hdfFile.readByte();
 			headerChecksum[1] = hdfFile.readByte();
 
-			hdfFile.skipBytes(1); // 06h: 0x00 maybe a filler
+			hdfFile.read(modelType);
 			
-			modelType[0] = hdfFile.readByte();
-			
-			hdfFile.skipBytes(1); // 04h: 0x00 maybe a filler
-			
-			numberBlocks[0]   = hdfFile.readByte();
+			hdfFile.read(numberBlocks);
 			
 			hdfFile.read(systemID1);
 			hdfFile.read(systemID2);
 
-			hdfFile.skipBytes(1); // 10h: 0x00 maybe a filler
-			
 			hdfFile.read(dataLength);
 			
+            // Calculate the actual Checksum of the header
+            // this value can than be compared with the given checksum (field 02h-03h)
+            CRC16 actualChecksum = new CRC16();
+            
+            byte[] headerData = new byte[getHeaderLength()-2];
+            hdfFile.seek(4);
+            hdfFile.read(headerData);
+            
+            actualChecksum.update(headerData,0,getHeaderLength()-2);
+            
+            headerChecksumCalc = (int)actualChecksum.getValue();
+            
  			hdfFile.close();
 		}
 		catch(FileNotFoundException e){}
@@ -81,17 +88,18 @@ public class HdfHeader
 	}
 	
 	/**
-	 * Gets the fHeaderLength.
+	 * Gets the headerLength.
 	 * @return Returns a int
 	 */
 	public int getHeaderLength()
 	{
-		return headerLength[0];
+		return (int) ((headerLength[0] <<8 & 0xFFFF)
+                      |(headerLength[1]     & 0xFF));
 	}
 
 		
 	/**
-	 * Gets the fHeaderChecksum.
+	 * Gets the headerChecksum.
 	 * @return Returns a int
 	 */
 	public int getHeaderChecksum()
@@ -102,17 +110,18 @@ public class HdfHeader
 
 		
 	/**
-	 * Gets the fModelType.
+	 * Gets the modelType.
 	 * @return Returns a int
 	 */
 	public int getModelType()
 	{
-		return modelType[0];
+		return (int) ((modelType[0] <<8 & 0xFFFF)
+                      |(modelType[1]     & 0xFF));
 	}
 
 		
 	/**
-	 * Gets the fSystemID1.
+	 * Gets the systemID1.
 	 * @return Returns a int
 	 */
 	public int getSystemID1()
@@ -125,7 +134,7 @@ public class HdfHeader
 
 		
 	/**
-	 * Gets the fSystemID2.
+	 * Gets the systemID2.
 	 * @return Returns a int
 	 */
 	public int getSystemID2()
@@ -138,24 +147,31 @@ public class HdfHeader
 
 		
 	/**
-	 * Gets the fNumberBlocks.
+	 * Gets the numberBlocks.
 	 * @return Returns a int
 	 */
 	public int getNumberBlocks()
 	{
-		return numberBlocks[0];
+		return (int) ((numberBlocks[0] <<8 & 0xFFFF)
+                      |(numberBlocks[1]     & 0xFF));
 	}
 
 	/**
-	 * Gets the fDataLength.
+	 * Gets the dataLength.
 	 * @return Returns a int
 	 */
 	public int getDataLength()
 	{
-		return (int) ((dataLength[0] <<16 & 0xFFFFFF)
-					  |(dataLength[1] <<8  & 0xFFFF)
-					  |(dataLength[2]      & 0xFF));
+		return (int) ((dataLength[0] <<24 & 0xFFFFFFFF)
+                      |(dataLength[1] <<16 & 0xFFFFFF)
+					  |(dataLength[2] <<8  & 0xFFFF)
+					  |(dataLength[3]      & 0xFF));
 	}
+
+    public boolean isHeaderChecksumValid()
+    {
+        return (getHeaderChecksum()==headerChecksumCalc);
+    }
 
 	public String toString()
 	{
@@ -163,7 +179,8 @@ public class HdfHeader
 		
 		StringBuffer report = new StringBuffer();
         report.append("Header length    : "   + getHeaderLength() + " Bytes\n");
-        report.append("Header Checksum  : 0x" + Integer.toHexString(getHeaderChecksum()).toUpperCase() + "\n");
+        report.append("Header Checksum  : 0x" + Integer.toHexString(getHeaderChecksum()).toUpperCase());
+        report.append(isHeaderChecksumValid() ? " (ok)\n" : "\n");
         report.append("Model Type       : "   + getModelType() + "\n");        
         report.append("System ID (1)    : ("  + Integer.toHexString(getSystemID1()) + ")\n");
         report.append("System ID (2)    : ("  + Integer.toHexString(getSystemID2()) + ")\n");        
